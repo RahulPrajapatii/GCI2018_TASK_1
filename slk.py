da7c66db72b11c1d6b1640e1531b435ef679f735
RahulPrajapatii <44726423+RahulPrajapatii@users.noreply.github.com>
2018-11-05 18:00:30 +0530

    Program file
    This is the program file

---
+++ slk.py
@@ -0,0 +1,8 @@
+r=0
+while r<=9:
+    print("GCI is great")
+    r=r+1
+    
+name=input("Enter Your Name : ")
+rname=name[::-1]
+print("Hello",name,",please to meet you! Did you know that your name backwards is",rname)
